89e895914f0e4da7ede49ca396d5e52ed3aca140
Jia Mu <jiamu0614@gmail.com>
2022-04-03 16:13:32 -0400

    add dto exports to common index.ts

--- packages/common/index.ts
+++ packages/common/index.ts
@@ -5,3 +5,8 @@ export * from "./types";
 export * from "./ib_exams";
 export * from "./ap_exams";
 export * from "./constants";
+export * from "./dto/create-plan.dto";
+export * from "./dto/create-student.dto";
+export * from "./dto/login-student.dto";
+export * from "./dto/update-plan.dto";
+export * from "./dto/update-student.dto";
